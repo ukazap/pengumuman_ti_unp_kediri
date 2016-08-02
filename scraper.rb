5f39b72f7ce4a91a2fe564c9eb44093f8316dbe2
Ukaza Perdana <ukazap@live.com>
2016-08-02 09:37:25 +0700

    pake ruby 2.3

--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,28 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+root = "http://informatika.ft.unpkediri.ac.id"
+page = agent.get("#{root}/?hal=arsip&kategori=Pengumuman")
 #
 # # Find somehing on the page using css selectors
 # p page.at('div.content')
+articles = page.search '.maincontent li'
 #
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+articles.each do |article|
+  ScraperWiki.save_sqlite ["link"], {
+    title: article.at('a').text,
+    link: root + '/' + article.at('a')[:href],
+    date: Date.parse article.at('small').text.split('|')[1].strip
+  }
+end
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
